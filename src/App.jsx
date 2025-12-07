c5ac4cf3848d3d9241ee75f19de642348a77c48f
Elio <eliocavalieri@gmail.com>
2025-12-07 19:49:29 -0300

    Etapa Proyecto Final React C_25235

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { AuthProvider } from './AuthContext';
+//import PrivateRoute from './PrivateRoute';
+//import Dashboard from './Dashboard';
+//mport Navbar from './NavBar';
 import Header from './components/Header';
 import Home from './components/Home';
 import Ofertas from './components/Ofertas';
 import Infaltables from './components/Infaltables';
-import Login from './components/Login'; 
+//import Login from './components/Login';
+//import Login from './Login';
+import Login from './components/Login';
 import Footer from './components/Footer'
 
 function App() {
@@ -12,7 +18,8 @@ function App() {
      <Router>
       <Header />
       <Routes>
-        <Route path="/administracion" element={<Login />} />
+        <Route path="*" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/ofertas" element={<Ofertas />} />
         <Route path="/infaltables" element={<Infaltables />} />
@@ -22,4 +29,28 @@ function App() {
   )
 }
 
-export default App
+/*
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+*/
+
+export default App;
